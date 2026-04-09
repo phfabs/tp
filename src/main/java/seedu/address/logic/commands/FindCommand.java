746ae2ee8c97a27590072513d724288b1417f7a6
memoflora <fathmubina09@gmail.com>
2026-04-09 15:57:40 +0800

    Fix find MESSAGE_USAGE to include remark and id fields

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -21,7 +21,7 @@ public class FindCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all members whose specific field contains "
             + "the specified query as a literal substring (case-insensitive).\n"
-            + "Supported fields: name, phone, email, emergency contact, and membership type.\n"
+            + "Supported fields: name, phone, email, emergency contact, membership type, member ID, and remark.\n"
             + "Parameters: QUERY\n"
             + "Example: " + COMMAND_WORD + " john doe";
 
